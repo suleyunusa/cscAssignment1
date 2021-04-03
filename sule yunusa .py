51f25a4dd9088eeb3bd886933e05926c438355e3
suleyunusa <81761734+suleyunusa@users.noreply.github.com>
2021-04-03 14:08:58 +0100

    Add files via upload

---
+++ sule yunusa .py
@@ -0,0 +1,30 @@
+print('Question ONE')
+NAME='SULE YUNUSA'
+DEPARTMENT='MATHEMATICS SCIENCE DEPARTMENT'
+print('my name is',NAME,'i am from',DEPARTMENT)
+print('Question TWO')
+scare='Boo!'
+
+print(scare)
+print('Question THREE')
+Numb1=7
+Numb2=9
+Numb3=Numb1 + Numb2
+print(Numb1,Numb2,Numb3)
+print('Question FOUR')
+clear=12/0.5
+print(clear)
+print('Question FIVE')
+Adding=5.1 + 6
+print(Adding)
+print('Question SIX')
+sure=8%3
+print(sure)
+print('Question SEVEN')
+Result1=2 * 4
+
+Result2=4 + 2
+
+Result3=Result1 * Result2
+
+print(Result3)
